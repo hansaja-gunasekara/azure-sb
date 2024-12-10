c964a0c11c678ba14bbcafcc4f6c827691eb904f
hansaja-gunasekara <hgmgunasekara@gmail.com>
2024-12-10 19:17:25 +0530

    Wrap NewTopNavBar with ProtectedRoute for enhanced access control

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import NewTopNavBar from "../components/NewTopNavBar";
 import NewFooter from "../components/NewFooter";
+import ProtectedRoute from "../components/ProtectedRoute";
 
 const Layout = () => {
   return (
     <>
-      <NewTopNavBar />
+      <ProtectedRoute>
+        <NewTopNavBar />
+      </ProtectedRoute>
       <Outlet />
       <NewFooter />
     </>
